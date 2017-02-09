b1e868a4e60d30c36ff055cc5cafe474a1e82780
pradyumnkumar <pradyumnkumarb@boston-technology.com>
2017-02-09 19:30:28 +0530

    Manage users get user list and activate and deactivate coding done in back end

--- fdahpStudyDesigner/src/main/java/com/fdahpStudyDesigner/controller/ManageUsersController.java
+++ fdahpStudyDesigner/src/main/java/com/fdahpStudyDesigner/controller/ManageUsersController.java
@@ -1,21 +1,87 @@
 package com.fdahpStudyDesigner.controller;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.apache.log4j.Logger;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.fdahpStudyDesigner.bo.UserBO;
 import com.fdahpStudyDesigner.service.ManageUsersService;
+import com.fdahpStudyDesigner.util.SessionObject;
+import com.fdahpStudyDesigner.util.fdahpStudyDesignerConstants;
+import com.fdahpStudyDesigner.util.fdahpStudyDesignerUtil;
 
 @Controller
 public class ManageUsersController {
 	
 	private static Logger logger = Logger.getLogger(ManageUsersController.class.getName());
-	
 	private ManageUsersService manageUsersService;
 
 	@Autowired
 	public void setManageUsersService(ManageUsersService manageUsersService) {
 		this.manageUsersService = manageUsersService;
 	}
-
+	@RequestMapping("/adminUsers/getUserList.do")
+	public ModelAndView getUserList(HttpServletRequest request){
+		logger.info("ManageUsersController - getUserList() - Starts");
+		ModelAndView mav = new ModelAndView();
+		ModelMap map = new ModelMap();
+		List<UserBO> userList = null;
+		String sucMsg = "";
+		String errMsg = "";
+		try{
+			if(fdahpStudyDesignerUtil.isSession(request)){
+				if(null != request.getSession().getAttribute("sucMsg")){
+					sucMsg = (String) request.getSession().getAttribute("sucMsg");
+					map.addAttribute("sucMsg", sucMsg);
+					request.getSession().removeAttribute("sucMsg");
+				}
+				if(null != request.getSession().getAttribute("errMsg")){
+					errMsg = (String) request.getSession().getAttribute("errMsg");
+					map.addAttribute("errMsg", errMsg);
+					request.getSession().removeAttribute("errMsg");
+				}
+				userList = manageUsersService.getUserList();
+				map.addAttribute("userList", userList);
+				mav = new ModelAndView("",map);
+			}
+		}catch(Exception e){
+			logger.error("ManageUsersController - getUserList() - ERROR",e);
+		}
+		logger.info("ManageUsersServiceImpl - getUserList() - Ends");
+		return mav;
+	}
+	
+	@RequestMapping("/adminUsers/activateOrDeactivateUser.do")
+	public void activateOrDeactivateUser(HttpServletRequest request,HttpServletResponse response,String userId,String userStatus) throws IOException{
+		logger.info("ManageUsersController - activateOrDeactivateUser() - Starts");
+		String msg = fdahpStudyDesignerConstants.FAILURE;
+		JSONObject jsonobject = new JSONObject();
+		PrintWriter out = null;
+		try{
+			HttpSession session = request.getSession();
+			SessionObject userSession = (SessionObject) session.getAttribute(fdahpStudyDesignerConstants.SESSION_OBJECT);
+			if(null != userSession){
+				msg = manageUsersService.activateOrDeactivateUser(Integer.valueOf(userId), Integer.valueOf(userStatus), userSession.getUserId());
+			}
+		}catch(Exception e){
+			logger.error("ManageUsersController - activateOrDeactivateUser() - ERROR",e);
+		}
+		logger.info("ManageUsersServiceImpl - activateOrDeactivateUser() - Ends");
+		jsonobject.put("message", msg);
+		response.setContentType("application/json");
+		out= response.getWriter();
+		out.print(jsonobject);
+	}
 }
--- fdahpStudyDesigner/src/main/java/com/fdahpStudyDesigner/dao/ManageUsersDAOImpl.java
+++ fdahpStudyDesigner/src/main/java/com/fdahpStudyDesigner/dao/ManageUsersDAOImpl.java
@@ -1,12 +1,19 @@
 package com.fdahpStudyDesigner.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.log4j.Logger;
 import org.hibernate.Query;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate3.HibernateTemplate;
 import org.springframework.stereotype.Repository;
+
+import com.fdahpStudyDesigner.bo.UserBO;
+import com.fdahpStudyDesigner.util.fdahpStudyDesignerConstants;
 
 @Repository
 public class ManageUsersDAOImpl implements ManageUsersDAO{
@@ -20,6 +27,68 @@ public class ManageUsersDAOImpl implements ManageUsersDAO{
 	public void setSessionFactory(SessionFactory sessionFactory) {
 		this.hibernateTemplate = new HibernateTemplate(sessionFactory);
 	}
-	
 
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<UserBO> getUserList() {
+		logger.info("ManageUsersDAOImpl - getUserList() - Starts");
+		Session session = null;
+		List<UserBO> userList = null;
+		List<Object[]> objList = null;
+		try{
+			session = hibernateTemplate.getSessionFactory().openSession();
+			query = session.createSQLQuery(" SELECT u.first_name,u.last_name,u.email,r.role_name FROM users u,roles r WHERE r.role_id = u.role_id ");
+			objList = query.list();
+			if(null != objList && objList.size() > 0){
+				userList = new ArrayList<UserBO>();
+				for(Object[] obj:objList){
+					UserBO userBO = new UserBO();
+					userBO.setFirstName(null != obj[0] ? String.valueOf(obj[0]) : "");
+					userBO.setLastName(null != obj[1] ? String.valueOf(obj[1]) : "");
+					userBO.setUserEmail(null != obj[2] ? String.valueOf(obj[2]) : "");
+					userBO.setRoleName(null != obj[3] ? String.valueOf(obj[3]) : "");
+					userList.add(userBO);
+				}
+			}
+		}catch(Exception e){
+			logger.error("ManageUsersDAOImpl - getUserList() - ERROR",e);
+		}finally{
+			if(null != session){
+				session.close();
+			}
+		}
+		logger.info("ManageUsersDAOImpl - getUserList() - Ends");
+		return userList;
+	}
+
+	@Override
+	public String activateOrDeactivateUser(int userId,int userStatus,int loginUser) {
+		logger.info("ManageUsersDAOImpl - activateOrDeactivateUser() - Starts");
+		String msg = fdahpStudyDesignerConstants.FAILURE;
+		Session session = null;
+		int count = 0;
+		try{
+			session = hibernateTemplate.getSessionFactory().openSession();
+			transaction = session.beginTransaction();
+			if(userStatus == 0){
+				userStatus = 1;
+			}else{
+				userStatus = 0;
+			}
+			query = session.createQuery(" UPDATE UserBO SET enabled = "+userStatus+" modifiedOn = now() AND modifiedBy = "+loginUser+" WHERE userId = "+userId );
+			count = query.executeUpdate();
+			transaction.commit();
+			if(count > 0){
+				msg = fdahpStudyDesignerConstants.SUCCESS;
+			}
+		}catch(Exception e){
+			logger.error("ManageUsersDAOImpl - activateOrDeactivateUser() - ERROR",e);
+		}finally{
+			if(null != session){
+				session.close();
+			}
+		}
+		logger.info("ManageUsersDAOImpl - activateOrDeactivateUser() - Ends");
+		return msg;
+	}
 }
