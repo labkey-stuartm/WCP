540f7035c030ce8739ee2e7c677ac8f7f8e268ac
PAWAN\Pawan <ronalins@boston-technology.com>
2017-02-21 16:41:17 +0530

    extra lines removed

--- fdahpStudyDesigner/src/main/java/com/fdahpStudyDesigner/controller/UsersController.java
+++ fdahpStudyDesigner/src/main/java/com/fdahpStudyDesigner/controller/UsersController.java
@@ -3,11 +3,9 @@ package com.fdahpStudyDesigner.controller;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
 import org.apache.log4j.Logger;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +13,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
 import com.fdahpStudyDesigner.bean.StudyListBean;
 import com.fdahpStudyDesigner.bo.RoleBO;
 import com.fdahpStudyDesigner.bo.UserBO;
