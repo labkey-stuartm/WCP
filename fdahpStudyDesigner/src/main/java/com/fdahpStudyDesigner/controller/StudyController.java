ebd6900ddf8367a08aeb438d53f9d0a8394eafdb
Vivek <viveka@boston-technology.com>
2017-03-01 14:01:35 +0530

    Added support for upload image

--- fdahpStudyDesigner/src/main/java/com/fdahpStudyDesigner/controller/StudyController.java
+++ fdahpStudyDesigner/src/main/java/com/fdahpStudyDesigner/controller/StudyController.java
@@ -270,8 +270,7 @@ public class StudyController {
 				}
 				if(studyBo.getFile()!=null && !studyBo.getFile().isEmpty()){
 					if(fdahpStudyDesignerUtil.isNotEmpty(studyBo.getThumbnailImage())){
-//						file = studyBo.getThumbnailImage().replace("."+studyBo.getThumbnailImage().split("\\.")[studyBo.getThumbnailImage().split("\\.").length - 1], "");
-						file = studyBo.getThumbnailImage().split("\\.")[0];
+						file = studyBo.getThumbnailImage().replace("."+studyBo.getThumbnailImage().split("\\.")[studyBo.getThumbnailImage().split("\\.").length - 1], "");
 					} else {
 						file = fdahpStudyDesignerUtil.getStandardFileName("STUDY",studyBo.getName(), studyBo.getCustomStudyId());
 					}
