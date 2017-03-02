53ae4f74df67c1055d5ff402bfaf5427bdc08277
PAWAN\Pawan <ronalins@boston-technology.com>
2017-03-02 14:39:56 +0530

    added overview page

--- fdahpStudyDesigner/src/main/java/com/fdahpStudyDesigner/controller/StudyController.java
+++ fdahpStudyDesigner/src/main/java/com/fdahpStudyDesigner/controller/StudyController.java
@@ -328,9 +328,9 @@ public class StudyController {
 					map.addAttribute("errMsg", errMsg);
 					request.getSession().removeAttribute("errMsg");
 				}
-				String studyId = (String) request.getSession().getAttribute("studyId");
-				if(studyId==null){
-					studyId = fdahpStudyDesignerUtil.isEmpty(request.getParameter("studyId")) == true? "0" :request.getParameter("studyId");
+				String  studyId = fdahpStudyDesignerUtil.isEmpty(request.getParameter("studyId")) == true? "" : request.getParameter("studyId");
+				if(fdahpStudyDesignerUtil.isEmpty(studyId)){
+					studyId = (String) request.getSession().getAttribute("studyId");
 				}
 				if(fdahpStudyDesignerUtil.isNotEmpty(studyId)){
 					studyBo = studyService.getStudyById(studyId, sesObj.getUserId());
@@ -445,7 +445,7 @@ public class StudyController {
 					if(fdahpStudyDesignerConstants.SUCCESS.equals(message)) {
 						request.getSession().setAttribute("sucMsg", "Setting and Admins set successfully.");
 						if(buttonText.equalsIgnoreCase(fdahpStudyDesignerConstants.COMPLETED_BUTTON))
-							 return new ModelAndView("redirect:viewSettingAndAdmins.do");
+							 return new ModelAndView("redirect:overviewStudyPages.do");
 							else
 								return new ModelAndView("redirect:viewSettingAndAdmins.do");
 					}else {
@@ -475,9 +475,9 @@ public class StudyController {
 			try{
 				SessionObject sesObj = (SessionObject) request.getSession().getAttribute(fdahpStudyDesignerConstants.SESSION_OBJECT);
 				if(sesObj!=null){
-					String studyId = (String) request.getSession().getAttribute("studyId");
-					if(StringUtils.isEmpty(studyId)){
-						studyId = fdahpStudyDesignerUtil.isEmpty(request.getParameter("studyId")) == true?"":request.getParameter("studyId");
+					String  studyId = fdahpStudyDesignerUtil.isEmpty(request.getParameter("studyId")) == true? "" : request.getParameter("studyId");
+					if(fdahpStudyDesignerUtil.isEmpty(studyId)){
+						studyId = (String) request.getSession().getAttribute("studyId");
 					}
 					if(StringUtils.isNotEmpty(studyId)){
 						studyPageBos = studyService.getOverviewStudyPagesById(studyId);
