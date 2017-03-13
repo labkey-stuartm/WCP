c897b3af8efb71d28f5572bebe0451a15014a858
PAWAN\Pawan <ronalins@boston-technology.com>
2017-03-13 19:26:51 +0530

    added for content active task

--- fdahpStudyDesigner/src/main/java/com/fdahpStudyDesigner/service/StudyActiveTasksServiceImpl.java
+++ fdahpStudyDesigner/src/main/java/com/fdahpStudyDesigner/service/StudyActiveTasksServiceImpl.java
@@ -10,9 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fdahpStudyDesigner.bo.ActiveTaskBo;
-import com.fdahpStudyDesigner.bo.QuestionnaireBo;
 import com.fdahpStudyDesigner.bo.StudyBo;
 import com.fdahpStudyDesigner.dao.StudyActiveTasksDAO;
+import com.fdahpStudyDesigner.util.SessionObject;
+import com.fdahpStudyDesigner.util.fdahpStudyDesignerUtil;
 
 /**
  * @author Vivek
@@ -45,5 +46,75 @@ public class StudyActiveTasksServiceImpl implements StudyActiveTasksService{
 		}
 		logger.info("StudyActiveTasksServiceImpl - getStudyActiveTasksByStudyId() - Ends");
 		return activeTasks;
+	}
+	/**
+	 * @author Ronalin
+	 * @param ActiveTaskBo
+	 * @return ActiveTaskBo
+	 * 
+	 * This method is used to save the activeTask
+	 */
+	@Override
+	public ActiveTaskBo saveOrUpdateActiveTask(ActiveTaskBo activeTaskBo,SessionObject sessionObject) {
+		logger.info("StudyActiveTasksServiceImpl - saveOrUpdateActiveTask() - Starts");
+		ActiveTaskBo updateActiveTaskBo = null;
+		try{
+			if(activeTaskBo != null){
+				if(activeTaskBo.getId() != null){
+					updateActiveTaskBo = studyActiveTasksDAO.getActiveTaskById(activeTaskBo.getId());
+					updateActiveTaskBo.setModifiedBy(sessionObject.getUserId());
+					updateActiveTaskBo.setModifiedOn(fdahpStudyDesignerUtil.getCurrentDateTime());
+				}else{
+					updateActiveTaskBo = new ActiveTaskBo();
+					updateActiveTaskBo.setCreatedBy(sessionObject.getUserId());
+					updateActiveTaskBo.setCreatedOn(fdahpStudyDesignerUtil.getCurrentDateTime());
+				}
+				updateActiveTaskBo = studyActiveTasksDAO.saveOrUpdateActiveTaskInfo(updateActiveTaskBo);
+			}
+			
+		}catch(Exception e){
+			logger.error("StudyActiveTasksServiceImpl - saveOrUpdateActiveTask() - Error",e);
+		}
+		logger.info("StudyActiveTasksServiceImpl - saveOrUpdateActiveTask() - Ends");
+		return updateActiveTaskBo;
+	}
+	
+	/**
+	 * @author Ronalin
+	 * @param Integer :aciveTaskId
+	 * @return Object :ActiveTaskBo
+	 * 
+	 * This method is used to get the ativeTask info object based on consent info id 
+	 */
+	@Override
+	public ActiveTaskBo getActiveTaskById(Integer ativeTaskId) {
+		logger.info("StudyActiveTasksServiceImpl - getActiveTaskById() - Starts");
+		ActiveTaskBo activeTask = null;
+		try {
+			activeTask = studyActiveTasksDAO.getActiveTaskById(ativeTaskId);
+		} catch (Exception e) {
+			logger.error("StudyActiveTasksServiceImpl - getActiveTaskById() - ERROR ", e);
+		}
+		logger.info("StudyActiveTasksServiceImpl - getActiveTaskById() - Ends");
+		return activeTask;
+	}
+	
+	/**
+	 * @author Ronalin
+	 * @param Integer : consentInfoId
+	 * @return String :SUCCESS or FAILURE
+	 *  TThis method used to get the delete the consent information
+	 */
+	@Override
+	public String deleteActiveTask(Integer activeTaskInfoId,Integer studyId) {
+		logger.info("StudyServiceImpl - deleteActiveTask() - Starts");
+		String message = null;
+		try{
+			message = studyActiveTasksDAO.deleteActiveTAsk(activeTaskInfoId, studyId);
+		}catch(Exception e){
+			logger.error("StudyServiceImpl - deleteActiveTask() - Error",e);
+		}
+		logger.info("StudyServiceImpl - deleteActiveTask() - Ends");
+		return message;
 	}
 }
