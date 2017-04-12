3b4c45a52a6c93934eff0f92d45ae26ed6665f95
pradyumnkumar <pradyumnkumarb@boston-technology.com>
2017-04-12 08:26:45 -0400

    audit log added

--- fdahpStudyDesigner/src/main/java/com/fdahpStudyDesigner/service/DashBoardAndProfileServiceImpl.java
+++ fdahpStudyDesigner/src/main/java/com/fdahpStudyDesigner/service/DashBoardAndProfileServiceImpl.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fdahpStudyDesigner.bo.UserBO;
+import com.fdahpStudyDesigner.dao.AuditLogDAO;
 import com.fdahpStudyDesigner.dao.DashBoardAndProfileDAO;
+import com.fdahpStudyDesigner.util.SessionObject;
 import com.fdahpStudyDesigner.util.fdahpStudyDesignerConstants;
 import com.fdahpStudyDesigner.util.fdahpStudyDesignerUtil;
 
@@ -25,17 +27,27 @@ public class DashBoardAndProfileServiceImpl implements DashBoardAndProfileServic
 	@Autowired
 	private DashBoardAndProfileDAO dashBoardAndProfiledao;
 	
+	@Autowired
+	private AuditLogDAO auditLogDAO;
+	
 	
 	/**
 	 * Kanchana
 	 * Updating User Details
 	 */
 	@Override
-	public String updateProfileDetails(UserBO userBO, int userId) {
+	public String updateProfileDetails(UserBO userBO, int userId,SessionObject userSession) {
 		logger.info("DashBoardAndProfileServiceImpl - updateProfileDetails - Starts");
 		String message = fdahpStudyDesignerConstants.FAILURE;
+		String activity = "";
+		String activityDetail = ""; 
 		try{
 				message = dashBoardAndProfiledao.updateProfileDetails(userBO, userId);
+				if(message.equals(fdahpStudyDesignerConstants.SUCCESS)){
+					activity = "Admin details updated";
+					activityDetail = "Admin changes in his own details is successfully updated";
+					auditLogDAO.saveToAuditLog(null, userSession, activity, activityDetail ,"DashBoardAndProfileDAOImpl - updateProfileDetails()");
+				}
 		}catch(Exception e){
 			logger.error("DashBoardAndProfileServiceImpl - updateProfileDetails() - Error",e);
 		}
