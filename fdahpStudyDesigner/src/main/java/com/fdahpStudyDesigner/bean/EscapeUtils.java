c5b044ef9b273596f9f0d45ac47c8f7b82faf95b
Vivek <viveka@boston-technology.com>
2017-04-18 18:47:58 +0530

    Added for code review

--- fdahpStudyDesigner/src/main/java/com/fdahpStudyDesigner/bean/EscapeUtils.java
+++ fdahpStudyDesigner/src/main/java/com/fdahpStudyDesigner/bean/EscapeUtils.java
@@ -5,49 +5,65 @@ import java.io.StringWriter;
 import java.io.Writer;
 import java.util.HashMap;
 
-public class EscapeUtils {
+import org.apache.log4j.Logger;
 
-	 public static final HashMap m = new HashMap();
-	    static {
-	        m.put(34, "&quot;"); // < - less-than
-	        m.put(60, "&lt;");   // < - less-than
-	        m.put(62, "&gt;");   // > - greater-than
-	    //User needs to map all html entities with their corresponding decimal values. 
-	     //Please refer to below table for mapping of entities and integer value of a char
-	    }
-	    public static String escapeHtml() {
-	        String str = "<script>alert(\"abc\")</script>";
-	        try {
-	            StringWriter writer = new StringWriter((int) 
-	                           (str.length() * 1.5));
-	            escape(writer, str);
-	            System.out.println("encoded string is " + writer.toString() );
-	            return writer.toString();
-	         } catch (IOException ioe) {
-	            ioe.printStackTrace();
-	            return null;
-	         }
-	    }
-	    public static void escape(Writer writer, String str) throws IOException {
-	         int len = str.length();
-	         for (int i = 0; i < len; i++) {
-                 char c = str.charAt(i);
-                 int ascii = (int) c;
-                 String entityName = (String) m.get(ascii);
-                 if (entityName == null) {
-                     if (c > 0x7F) {
-                         writer.write("&#");
-                         writer.write(Integer.toString(c, 10));
-                         writer.write(';');
-                     } else {
-                         writer.write(c);
-                     }
-                 } else {
-                          writer.write(entityName);
-                 }
-             }      
-	    }
-	    public static void main(String[] args) {
-        	escapeHtml();
-        }        
+import com.fdahpStudyDesigner.util.fdahpStudyDesignerUtil;
+
+public class EscapeUtils {
+	private static Logger logger = Logger.getLogger(EscapeUtils.class.getName());
+	
+	EscapeUtils(){
+		
+	}
+
+	protected static final HashMap m = new HashMap();
+	static {
+		m.put(34, "&quot;"); // < - less-than
+		m.put(60, "&lt;"); // < - less-than
+		m.put(62, "&gt;"); // > - greater-than
+		// User needs to map all html entities with their corresponding decimal
+		// values.
+		// Please refer to below table for mapping of entities and integer value
+		// of a char
+	}
+
+	public static String escapeHtml() {
+		logger.info("EscapeUtils - escapeHtml :: Starts");
+		String str = "<script>alert(\"abc\")</script>";
+		String writerStr = null;
+		try {
+			StringWriter writer = new StringWriter((int) (str.length() * 1.5));
+			escape(writer, str);
+			logger.info("EscapeUtils - escapeHtml - encoded string is " + writer.toString());
+			writerStr = writer.toString();
+		} catch (Exception e) {
+			logger.error("EscapeUtils - escapeHtml - ERROR ", e);
+		}
+		logger.info("EscapeUtils - escapeHtml :: Ends");
+		return writerStr;
+	}
+
+	public static void escape(Writer writer, String str) throws IOException {
+		int len = str.length();
+		for (int i = 0; i < len; i++) {
+			char c = str.charAt(i);
+			int ascii = (int) c;
+			String entityName = (String) m.get(ascii);
+			if (entityName == null) {
+				if (c > 0x7F) {
+					writer.write("&#");
+					writer.write(Integer.toString(c, 10));
+					writer.write(';');
+				} else {
+					writer.write(c);
+				}
+			} else {
+				writer.write(entityName);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		escapeHtml();
+	}  
 }
