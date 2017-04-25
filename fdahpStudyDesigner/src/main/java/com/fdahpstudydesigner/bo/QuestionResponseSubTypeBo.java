95c6ffdb214dfdec22ba9d97272f91fb4f2aa6ca
Vivek <viveka@boston-technology.com>
2017-04-25 15:48:19 +0530

    sonarqube code enhancement

--- fdahpStudyDesigner/src/main/java/com/fdahpstudydesigner/bo/QuestionResponseSubTypeBo.java
+++ fdahpStudyDesigner/src/main/java/com/fdahpstudydesigner/bo/QuestionResponseSubTypeBo.java
@@ -1,14 +1,12 @@
 package com.fdahpstudydesigner.bo;
 
 import java.io.Serializable;
-import java.sql.Blob;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Lob;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
