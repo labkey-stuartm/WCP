9a6633cfb6414705b63ae4c848e95f16de7e3a8a
kanchanar <kanchanar@boston-technology.com>
2017-04-26 07:08:38 -0400

    updated

--- fdahpStudyDesigner/src/main/java/com/fdahpstudydesigner/util/FdahpStudyDesignerConstants.java
+++ fdahpStudyDesigner/src/main/java/com/fdahpstudydesigner/util/FdahpStudyDesignerConstants.java
@@ -260,7 +260,6 @@ public class FdahpStudyDesignerConstants {
 	//Notification flags
 		public static final String NOTIFICATIONID = "notificationId";
 		public static final String CHKREFRESHFLAG = "chkRefreshflag";
-		public static final String REDIRECTTONOTIFICATIONLIST = "redirect:/adminNotificationView/viewNotificationList.do";
 		public static final String ADDORCOPY = "addOrCopy";
 		public static final String RESEND = "resend";
 }
