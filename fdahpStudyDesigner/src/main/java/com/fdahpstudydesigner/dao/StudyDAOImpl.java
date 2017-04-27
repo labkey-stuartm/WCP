1273f7cf60f361b2e46b96573e23fb54af5df597
pradyumnkumar <pradyumnkumarb@boston-technology.com>
2017-04-27 01:48:07 -0400

    conflicts resolved

--- fdahpStudyDesigner/src/main/java/com/fdahpstudydesigner/dao/StudyDAOImpl.java
+++ fdahpStudyDesigner/src/main/java/com/fdahpstudydesigner/dao/StudyDAOImpl.java
@@ -96,27 +96,16 @@ public class StudyDAOImpl implements StudyDAO{
 						+ " and p.userId=:impValue"
 						+ " order by s.createdOn desc");
 				query.setParameter("impValue", userId);
-<<<<<<< HEAD
-				StudyListBeans = query.list();
-				if(StudyListBeans!=null && !StudyListBeans.isEmpty()){
-					for(StudyListBean bean:StudyListBeans){
-=======
+				
 				studyListBeans = query.list();
 				if(studyListBeans != null && !studyListBeans.isEmpty()){
 					for(StudyListBean bean:studyListBeans){
-							/*query = session.createSQLQuery("select CONCAT(u.first_name,' ',u.last_name) AS name" 
-                                                           +" from users u where u.user_id in(select s.project_lead"
-                                                           +" from study_permission s where s.study_id="+bean.getId()
-                                                           +" and s.project_lead IS NOT NULL"
-                                                           + " and s.delFlag IS NOT NULL ");r
-							name = (String) query.uniqueResult();*/
->>>>>>> df25c191d454a63be04c0bd59c4d36009c4272ed
 							if(StringUtils.isNotEmpty(name))
 								bean.setProjectLeadName(name);
 							if(StringUtils.isNotEmpty(bean.getCategory()) && StringUtils.isNotEmpty(bean.getResearchSponsor())){
 								query = session.createQuery("from ReferenceTablesBo where id in("+bean.getCategory()+","+bean.getResearchSponsor()+")");
 								referenceTablesBos =query.list();
-								if(referenceTablesBos!=null && referenceTablesBos.size()>0){
+								if(referenceTablesBos!=null && !referenceTablesBos.isEmpty()){
 									bean.setCategory(referenceTablesBos.get(0).getValue());
 									bean.setResearchSponsor(referenceTablesBos.get(1).getValue());
 								}
