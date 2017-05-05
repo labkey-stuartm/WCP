781f236d62e752236e4b098a09266f6f7b5dda7c
Vivek <viveka@boston-technology.com>
2017-05-05 13:52:24 +0530

    #13536 , #13534, #13508

--- fdahpStudyDesigner/src/main/webapp/js/common.js
+++ fdahpStudyDesigner/src/main/webapp/js/common.js
@@ -45,13 +45,13 @@ function checkboxValidate(name){
     }
 }
 $(document).ready(function(){
-//	$("select[multiple='multiple']").on('change', function(e){
-//		if(($(this).val()).length){
-//			$(this).prop('required',false);
-//		} else {
-//			$(this).prop('required',true);
-//		}
-//     });
+	$("select[multiple='multiple']").on('change', function(e){
+		if(($(this).val()).length){
+			$(this).prop('required',false);
+		} else {
+			$(this).prop('required',true);
+		}
+     });
 	$("button[type = submit]").on('click', function(e){
 		if($( this ).hasClass( "disabled" )){
 			e.preventDefault();
