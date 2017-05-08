9eb0655849976b6e6fc704eef46b0fd647473ca8
RavinderV <ravinderv@boston-technology.com>
2017-05-08 19:06:41 +0530

    consent review issue fixed

--- fdahpStudyDesigner/src/main/java/com/fdahpstudydesigner/controller/StudyController.java
+++ fdahpStudyDesigner/src/main/java/com/fdahpstudydesigner/controller/StudyController.java
@@ -1424,9 +1424,9 @@ public class StudyController {
 				if(StringUtils.isEmpty(studyId)){
 					studyId = StringUtils.isEmpty(request.getParameter(FdahpStudyDesignerConstants.STUDY_ID))?"":request.getParameter(FdahpStudyDesignerConstants.STUDY_ID);
 				}
-				if(StringUtils.isEmpty(consentId)){
+				/*if(StringUtils.isEmpty(consentId)){
 					consentId = StringUtils.isEmpty(request.getParameter(FdahpStudyDesignerConstants.CONSENT_ID))?"":request.getParameter(FdahpStudyDesignerConstants.CONSENT_ID);
-				}
+				}*/
 				//Added for live version Start
 				String isLive = (String) request.getSession().getAttribute(FdahpStudyDesignerConstants.IS_LIVE);
 				if(StringUtils.isNotEmpty(isLive) && isLive.equalsIgnoreCase(FdahpStudyDesignerConstants.YES)){
@@ -1463,7 +1463,7 @@ public class StudyController {
 					}
 				}
 				map.addAttribute(FdahpStudyDesignerConstants.STUDY_ID, studyId);
-				map.addAttribute(FdahpStudyDesignerConstants.CONSENT_ID, consentId);
+				//map.addAttribute(FdahpStudyDesignerConstants.CONSENT_ID, consentId);
 				map.addAttribute("consentBo", consentBo);
 				mav = new ModelAndView("consentReviewAndEConsentPage", map);
 			}
