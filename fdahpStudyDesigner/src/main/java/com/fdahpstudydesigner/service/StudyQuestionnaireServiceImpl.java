d7e7d8c0515f86d0e35dda99fe48a6bbe03f445e
RavinderV <ravinderv@boston-technology.com>
2017-07-12 12:46:54 +0530

    questionnaire done btn load time improvements

--- fdahpStudyDesigner/src/main/java/com/fdahpstudydesigner/service/StudyQuestionnaireServiceImpl.java
+++ fdahpStudyDesigner/src/main/java/com/fdahpstudydesigner/service/StudyQuestionnaireServiceImpl.java
@@ -250,6 +250,9 @@ public class StudyQuestionnaireServiceImpl implements StudyQuestionnaireService{
 				if(questionnaireBo.getPreviousFrequency() != null){
 					addQuestionnaireBo.setPreviousFrequency(questionnaireBo.getPreviousFrequency());
 				}
+				if(questionnaireBo.getCurrentFrequency() != null){
+					addQuestionnaireBo.setCurrentFrequency(questionnaireBo.getCurrentFrequency());
+				}
 				addQuestionnaireBo.setIsChange(questionnaireBo.getIsChange());
 				addQuestionnaireBo = studyQuestionnaireDAO.saveORUpdateQuestionnaire(addQuestionnaireBo, sessionObject,customStudyId);
 			}
